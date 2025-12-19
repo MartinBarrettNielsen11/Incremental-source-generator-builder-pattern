4702ebc2a22aa4485d7e9e0ebe4d5a5d6de83ca6
MartinBarrettNielsen11 <76744292+MartinBarrettNielsen11@users.noreply.github.com>
2025-12-19 22:45:10 +0100

    inherit IncrementalGenerator abstraction

--- Incremental-source-generator-builder-pattern/Generator.cs
+++ Incremental-source-generator-builder-pattern/Generator.cs
@@ -1,7 +1,18 @@
+using Microsoft.CodeAnalysis;
+
 namespace Incremental_source_generator_builder_pattern;
 
 /// <summary>
 /// Incremental source generator for creating Builder-classes.
 /// </summary>
 
-public class Generator;
+#pragma warning disable RS1038
+[Generator(LanguageNames.CSharp)]
+#pragma warning restore RS1038
+internal sealed class BuilderGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}

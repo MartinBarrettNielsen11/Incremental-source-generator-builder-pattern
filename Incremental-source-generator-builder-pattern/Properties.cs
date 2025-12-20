f234878bb496f6bcd15ecdc81ae5450653c0a701
MartinBarrettNielsen11 <76744292+MartinBarrettNielsen11@users.noreply.github.com>
2025-12-20 02:50:22 +0100

    Add readonly struct for carrying properties of different types

--- Incremental-source-generator-builder-pattern/Properties.cs
+++ Incremental-source-generator-builder-pattern/Properties.cs
@@ -1,3 +1,16 @@
+using Incremental_source_generator_builder_pattern.Contracts;
+
 namespace Incremental_source_generator_builder_pattern;
 
-internal readonly struct Properties();
+/// <summary>
+/// Helper class for holding properties used for code generation
+/// </summary>
+internal readonly struct Properties(
+    List<PropertyInfoModel> normal,
+    List<PropertyInfoModel> collection)
+{
+    public List<PropertyInfoModel> Normal { get; } = normal;
+    public List<PropertyInfoModel> Collection { get; } = collection;
+
+    public List<PropertyInfoModel> AllProperties => new(Normal.Concat(Collection).ToArray());
+}
